e122730d05eaa68309ad8a7d26e8a687e0b1d1e5
Robert Krause <Ruport@web.de>
2013-11-02 08:15:14 +0100

    added support for USB, SERIAL and BLUETOOTH link types

--- pcap.c
+++ pcap.c
@@ -230,7 +230,7 @@ int fix_pcap(FILE *pcap, FILE *pcap_fix) {
   }
 
   /* check for data link type (http://www.tcpdump.org/linktypes.html) */
-  if (conint(global_hdr.network) <= 245) {	/* data link types are <= 245 */
+  if (conint(global_hdr.network) <= 251) {	/* data link types are <= 251 */
     if (verbose) printf("[+] Data link type: %u\n", conint(global_hdr.network));
   } else {
     hdr_integ++;
