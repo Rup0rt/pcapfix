e6671895c52866fb01fcb59d87d97fcd0d5a974b
Robert Krause <Ruport@web.de>
2019-02-02 18:16:22 +0100

    put win function into sep file to avoid multi incl

---
+++ winfunc.c
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2012-2019 Robert Krause (ruport@f00l.de)
+ *
+ * This file is part of Pcapfix.
+ *
+ * Pcapfix is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * Pcapfix is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Pcapfix. If not, see http://www.gnu.org/licenses/.
+ *
+ ******************************************************************************/
+
+#include "pcapfix.h"
+
+#include <fcntl.h>
+
+/* truncate does not exist under windows */
+int truncate(const char *pathname, _off_t len){
+  int ret, err;
+  int fd = _open(pathname,_O_BINARY|_O_RDWR);
+  if (fd == -1) return fd;
+  ret = ftruncate(fd,len);
+  _close(fd);
+  return ret;
+}
